f946f2640247e87489ca6735b5aca0047c90fe05
k7areef <kh3reef@gmail.com>
2026-01-06 15:43:11 +0200

    feat: update HTML structure, add Google Fonts, and enhance CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ import Footer from "@components/layout/Footer";
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App text-gray-900'>
       {/* Header */}
       <Header />
       {/* Routes */}
