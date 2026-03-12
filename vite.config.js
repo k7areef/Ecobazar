56da47b57edfd50c57f1e9b05414bf33949db739
k7areef <kh3reef@gmail.com>
2026-03-12 11:47:04 +0200

    Add Apis

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
   },
 })
