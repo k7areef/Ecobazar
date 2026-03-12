3dcfd9b6a74bbb00815b86dbc1c0440230c10f1a
k7areef <kh3reef@gmail.com>
2026-03-12 11:53:03 +0200

    Disabled Subscribe in Home Page

--- src/components/layout/Subscribe/index.jsx
+++ src/components/layout/Subscribe/index.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router-dom";
+
 function Subscribe() {
+
+    const { pathname } = useLocation();
+    if (pathname === '/') return null;
+
     return (
         <div className="subscribe">
             <div className="container">
