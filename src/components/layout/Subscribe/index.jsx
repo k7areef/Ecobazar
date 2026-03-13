4a6edb39d69ef19f316559e919d687a0c78b32ce
k7areef <kh3reef@gmail.com>
2026-03-13 07:30:26 +0200

    feat: Complete Subscribe Section

--- src/components/layout/Subscribe/index.jsx
+++ src/components/layout/Subscribe/index.jsx
@@ -1,4 +1,15 @@
+import Button from "@components/UI/Button";
 import { useLocation } from "react-router-dom";
+import SocialProfiles from "@data/social_profiles.json";
+import { faFacebook, faGithub, faLinkedin, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const iconsMaper = {
+    facebook: faFacebook,
+    github: faGithub,
+    linkedin: faLinkedin,
+    xTwitter: faXTwitter
+};
 
 function Subscribe() {
 
@@ -6,11 +17,51 @@ function Subscribe() {
     if (pathname === '/') return null;
 
     return (
-        <div className="subscribe">
+        <section className="subscribe bg-[#F7F7F7] py-5 mb:py-10" id="subscribe">
             <div className="container">
-                Subscribe
+                <div className="content-container flex lg:items-center gap-5 lg:gap-10 max-lg:flex-col">
+                    <div className="text-wrapper w-full">
+                        <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mb-1.5">Subscribe our Newsletter</h2>
+                        <p className="text-grey-400!">Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna.</p>
+                    </div>
+                    <div className="input-wrapper w-full flex items-center gap-3 max-sm:flex-col">
+                        {/* Subscribe Form */}
+                        <form onSubmit={e => e.preventDefault()} className="relative w-full">
+                            <input
+                                required
+                                type="email"
+                                id="subscribe"
+                                name="subscribe"
+                                placeholder="Your email address"
+                                className="w-full py-3 px-6 rounded-full border-2 border-grey-100"
+                            />
+                            <Button
+                                type="submit"
+                                title="Subscribe"
+                                aria-label="Subscribe"
+                                className="rounded-full absolute z-1 right-0 top-0 h-full"
+                            >
+                                Subscribe
+                            </Button>
+                        </form>
+                        {/* Social Profiles */}
+                        <div className="social-profiles flex items-center gap-2 shrink-0">
+                            {
+                                SocialProfiles.map((profile, index) => (<Button
+                                    variant="ghost"
+                                    href={profile.href}
+                                    key={profile.id || index}
+                                    className="rounded-full p-0! w-12 h-12 flex items-center justify-center text-2xl sm:hover:bg-primary sm:hover:text-white"
+                                >
+                                    <FontAwesomeIcon icon={iconsMaper[profile.icon || faGithub]} />
+                                    <span className="sr-only"></span>
+                                </Button>))
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@data': path.resolve(__dirname, './src/data'),
     },
   },
 })
