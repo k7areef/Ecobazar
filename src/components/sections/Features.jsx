c737300955e90312f1c8414d0421a5346c1a19a6
k7areef <kh3reef@gmail.com>
2026-03-24 09:47:28 +0200

    fix(Features.jsx): write replace p3 with p-3

--- src/components/sections/Features.jsx
+++ src/components/sections/Features.jsx
@@ -17,7 +17,7 @@ function Features() {
                 {/* Features */}
                 <div className="features-grid grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
                     {
-                        features.map((feature, index) => (<div className="feature-card p3 md:p-5 text-center border-b-2 border-b-grey-100 transition-colors duration-200 sm:hover:border-b-primary" key={index}>
+                        features.map((feature, index) => (<div className="feature-card p-3 md:p-5 text-center border-b-2 border-b-grey-100 transition-colors duration-200 sm:hover:border-b-primary" key={index}>
                             <div className="feature-image mx-auto w-16 h-16 rounded-full flex items-center justify-center bg-primary/15 text-2xl text-primary mb-3">
                                 <FontAwesomeIcon icon={iconsMaper[feature.icon]} />
                             </div>
