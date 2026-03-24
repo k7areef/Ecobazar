423dcdf3084f4c1141e3fcfecf949663d909ea1c
k7areef <kh3reef@gmail.com>
2026-03-24 08:17:47 +0200

    feat: add className prop to header section

--- src/components/sections/shared/SectionHeader.jsx
+++ src/components/sections/shared/SectionHeader.jsx
@@ -3,6 +3,7 @@
  * @prop {string} [beforeTtitle]
  * @prop {string} title
  * @prop {string} [description]
+ * @prop {string} [className]
  * @prop {React.ReactNode} [children]
  */
 
@@ -10,9 +11,9 @@
  * @param {SectionHeaderProps} props
  */
 
-function SectionHeader({ beforeTtitle, title, description, children }) {
+function SectionHeader({ beforeTtitle, title, description, children, className }) {
     return (
-        <div className="section-header flex sm:items-center justify-between mb-5 md:mb-10 max-md:flex-wrap max-sm:flex-col gap-3">
+        <div className={`section-header flex sm:items-center justify-between mb-5 md:mb-10 max-md:flex-wrap max-sm:flex-col gap-3 ${className}`}>
             <div className="text-wrapper w-full">
                 {beforeTtitle && <span className="uppercase text-primary italic">{beforeTtitle}</span>}
                 <h2 className="font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">{title}</h2>
