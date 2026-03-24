ce37e85263101c158dfc015cecc6e9edcceb0ca9
k7areef <kh3reef@gmail.com>
2026-03-24 20:19:09 +0200

    feat: add loading state to user profile context

--- src/contexts/providers/UserProfileContext.jsx
+++ src/contexts/providers/UserProfileContext.jsx
@@ -6,20 +6,23 @@ const UserProfileContext = React.createContext();
 
 export const UserProfileContextProvider = ({ children }) => {
 
-    const { isAuth } = useAuth();
+    const { isAuth, authLoading } = useAuth();
 
     const [profile, setProfile] = React.useState(null);
+    const [loading, setLoading] = React.useState(authLoading);
 
     React.useEffect(() => {
         if (!isAuth) return;
-        GET_USER_PROFILE().then(res => {
+        setLoading(true);
+        GET_USER_PROFILE().then(res => { // Get User Profile
             setProfile(res.data);
-        })
+        }).finally(() => setLoading(false));
     }, [isAuth]);
 
     return (
         <UserProfileContext.Provider value={{
-            profile
+            profile,
+            loading
         }}>
             {children}
         </UserProfileContext.Provider>
