ff220450c2e5d8c05c41f75ad722876fd96c2999
k7areef <kh3reef@gmail.com>
2026-03-24 06:39:53 +0200

    feat: add supbase client

---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,7 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const publishableKey = import.meta.env.PUBLISHABLE_DEFAULT_KEY
+
+export const supabase = createClient(supabaseUrl, publishableKey)
