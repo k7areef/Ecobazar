3180d1b0cd0422321a02b449dc3b8079d089e4d4
k7areef <kh3reef@gmail.com>
2026-03-24 07:15:59 +0200

    feat(contexts): add auth context

---
+++ src/contexts/providers/AuthContext.jsx
@@ -0,0 +1,25 @@
+import { supabase } from "@utils/supabaseClient";
+import React from "react";
+
+const AuthContext = React.createContext();
+
+export const AuthContextProvider = ({ children }) => {
+
+    const [isAuth, setIsAuth] = React.useState(false);
+
+    React.useEffect(() => {
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
+            const isLoggedIn = !!session?.user
+            setIsAuth(isLoggedIn);
+        })
+        subscription.unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{
+            isAuth
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
