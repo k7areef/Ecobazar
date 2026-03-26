62313d2df02c28861c9aa2aa4dba88042f40e1b2
k7areef <kh3reef@gmail.com>
2026-03-27 01:39:58 +0200

    feat: update ProductCardSkeleton and ProductsPage for improved loading states and data fetching
    - Changed background color in ProductCardSkeleton for better visual consistency.
    - Integrated React Query in ProductsPage to fetch product data from Supabase, enhancing content visibility.
    - Updated ProductsPage layout to include ProductsGrid for displaying fetched products with loading states.

--- src/components/products/ProductCardSkeleton.jsx
+++ src/components/products/ProductCardSkeleton.jsx
@@ -1,15 +1,15 @@
 function ProductCardSkeleton() {
     return (
         <div className="product-card-skeleton border border-grey-100 rounded-lg overflow-hidden">
-            <div className="aspect-square bg-primary/30 animate-pulse"></div>
+            <div className="aspect-square bg-grey-100 animate-pulse"></div>
             <div className="p-3 md:p-5 flex items-center justify-between">
                 <div className="h-20.5 flex flex-col gap-2">
-                    <div className="h-full rounded-sm bg-primary/30 animate-pulse w-30"></div>
-                    <div className="h-full rounded-sm bg-primary/30 animate-pulse w-15"></div>
+                    <div className="h-full rounded-sm bg-grey-100 animate-pulse w-30"></div>
+                    <div className="h-full rounded-sm bg-grey-100 animate-pulse w-15"></div>
                     <ul className="h-full flex items-center gap-1">
                         {
                             Array.from({ length: 5 }).map((_, index) => (
-                                <li key={index} className="w-5 h-5 rounded-full bg-primary/30 animate-pulse"></li>
+                                <li key={index} className="w-5 h-5 rounded-full bg-grey-100 animate-pulse"></li>
                             ))
                         }
                     </ul>
--- src/pages/main/ProductsPage.jsx
+++ src/pages/main/ProductsPage.jsx
@@ -1,10 +1,28 @@
 import ProductQuickViewModal from "@components/modals/ProductQuickViewModal";
+import ProductsGrid from "@components/products/ProductsGrid";
 import ProductsHeader from "@components/products/ProductsHeader";
 import ProductsSidebar from "@components/products/ProductsSidebar";
 import useChangeTitle from "@hooks/useChangeTitle";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@utils/supabaseClient";
 
 function ProductsPage() {
     useChangeTitle({ title: 'Products' });
+
+    const LIMIT = 15;
+
+    const { data, isLoading } = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+            const { data, error } = await supabase
+                .from("products")
+                .select("*")
+                .limit(LIMIT);
+            if (error) throw error;
+            return data;
+        }
+    });
+
     return (
         <div className="products-page py-5 md:py-10">
             <div className="container">
@@ -16,7 +34,7 @@ function ProductsPage() {
                     <ProductsSidebar />
                     {/* Main Content */}
                     <main className="main-content flex-1 min-w-0">
-                        Main Content
+                        <ProductsGrid isLoading={isLoading} products={data || []} limit={LIMIT} />
                     </main>
                 </div>
             </div>
