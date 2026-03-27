ef39728f14b15dd4dbcf957a017bf5c09f177839
k7areef <kh3reef@gmail.com>
2026-03-28 01:16:44 +0200

    feat: integrate pagination in ProductsPage for enhanced product navigation
    - Added Pagination component to ProductsPage, allowing users to navigate through product listings more efficiently.
    - Updated data fetching logic to support pagination, including total product count and range selection.
    - Enhanced ProductsGrid to display products based on the current page, improving user experience and maintainability.

--- src/pages/main/ProductsPage.jsx
+++ src/pages/main/ProductsPage.jsx
@@ -1,3 +1,4 @@
+import Pagination from "@components/common/Pagination";
 import ProductQuickViewModal from "@components/modals/ProductQuickViewModal";
 import ProductsGrid from "@components/products/ProductsGrid";
 import ProductsHeader from "@components/products/ProductsHeader";
@@ -5,33 +6,50 @@ import ProductsSidebar from "@components/products/ProductsSidebar";
 import useChangeTitle from "@hooks/useChangeTitle";
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@utils/supabaseClient";
+import React from "react";
 import { useSearchParams } from "react-router-dom";
 
 function ProductsPage() {
     useChangeTitle({ title: 'Products' });
 
+    const [currentPage, setCurrentPage] = React.useState(1);
     const LIMIT = 15;
+
+    const from = (currentPage - 1) * LIMIT;
+    const to = from + LIMIT - 1;
 
     const [searchParams] = useSearchParams();
     const CATEGORY_PARAM = searchParams.get('category') || 'all';
 
     const { data, isLoading } = useQuery({
-        queryKey: ['products', CATEGORY_PARAM],
+        queryKey: ['products', CATEGORY_PARAM, currentPage],
         queryFn: async () => {
             let query = supabase
                 .from("products")
-                .select("*")
+                .select("*", { count: "exact" })
+                .range(from, to)
                 .limit(LIMIT);
 
             if (CATEGORY_PARAM !== "all") {
                 query = query.eq('category_id', CATEGORY_PARAM);
             }
 
-            const { data, error } = await query;
+            const { data, error, count } = await query;
             if (error) throw error;
-            return data;
+            return { products: data, totalCount: count };
         }
     });
+
+    const totalPages = Math.ceil((data?.totalCount || 0) / LIMIT);
+
+    const handleNext = React.useCallback(() => {
+        if (currentPage < totalPages) setCurrentPage(prev => prev + 1);
+    }, [currentPage, totalPages]);
+
+    const handlePrev = React.useCallback(() => {
+        if (currentPage > 1) setCurrentPage(prev => prev - 1);
+    }, [currentPage]);
+
 
     return (
         <div className="products-page py-5 md:py-10">
@@ -44,7 +62,19 @@ function ProductsPage() {
                     <ProductsSidebar />
                     {/* Main Content */}
                     <main className="main-content flex-1 min-w-0">
-                        <ProductsGrid isLoading={isLoading} products={data || []} limit={LIMIT} className="2xl:grid-cols-3!" />
+                        <ProductsGrid isLoading={isLoading} products={data?.products || []} limit={LIMIT} className="2xl:grid-cols-3!" />
+                        <Pagination
+                            current={currentPage}
+                            pageCount={totalPages}
+                            className="justify-center p-5"
+                            handleNext={handleNext}
+                            handlePageChange={(page) => {
+                                setCurrentPage(page);
+                            }}
+                            handlePrev={handlePrev}
+                            nextDisabled={currentPage >= totalPages}
+                            prevDisabled={currentPage <= 1}
+                        />
                     </main>
                 </div>
             </div>
